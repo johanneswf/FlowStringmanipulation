70e442f35c57a98e8bc1492f3836a0020b08cc53
johanneswf <johanneswf@gmail.com>
2022-05-12 18:40:49 +0200

    Added some comments but the program is basic enough.

--- FlowStringmanipulation/Program.cs
+++ FlowStringmanipulation/Program.cs
@@ -7,82 +7,107 @@ namespace FlowStringmanipulation
     {
         static void Main(string[] args)
         {
-            bool run = true;
+            bool run = true; // Loop-brytare.
             do
             {
+                // Eftersom menyn endast används här behöver vi inte skapa en metod för att skapa den.
                 Console.Clear();
                 Console.WriteLine("Välkommen till BIO");
                 Console.WriteLine("0. Stäng av programmet");
                 Console.WriteLine("1. Köp enskild biljett");
                 // Jag hade haft ETT menyval för biljettköp, om man vill köpa en biljett kan man välja 1 biljett helt enkelt.
-                Console.WriteLine("1b. Köp biljetter för sällskap (malicious compliance)");
+                Console.WriteLine("1b. Köp biljetter för sällskap");
+                // Lite malicious compliance här.
                 // Enligt specifikation ville ni att case 2 skulle vara upprepning så då får sällskapsbiljettköpet hamna osmidigt.
                 // I vanliga fall pratar vi med klienten om detta.
                 Console.WriteLine("2. Upprepningar");
                 Console.WriteLine("3. Splitta sträng");
                 Console.WriteLine();
-                Console.Write("Val: ");
+                Console.Write("Menyval: "); //Jag gillar att berätta för användaren vad vi håller på med i inputbuffern.
 
                 string huvudmenyVal = Console.ReadLine();
                 switch (huvudmenyVal)
                 {
                     case "0":
-                        run = false;
+                        run = false; //Loop-brytare, stänger av programmet.
                         break;
+
                     case "1":
                         Console.Clear();
-                        KöpBiljett();
+                        KöpBiljett(); //Egna metoderna finns efter Main-metoden.
                         VäntaPåAnvändare();
                         break;
-                    case "1b":
+
+                    case "1b": // Specifikation sa både att vi skulle skapa en case för detta samt att case 2 skulle vara något
+                        // helt annat så vi kompromissar med att döpa detta val till 1b.
                         Console.Clear();
                         Console.WriteLine("Köp biljetter för sällskap");
-                        Console.Write("Antal besökare: ");
-                        Sällskap(ValideraUINT());
+                        Sällskap(ValideraUINT("Antal besökare: "));
                         VäntaPåAnvändare();
                         break;
+
                     case "2":
                         Console.WriteLine("Skriv in något du vill upprepa 10 gånger");
                         Console.Write("Upprepa: ");
                         string upprepa = Console.ReadLine();
                         uint upprepningar = 0;
                         while (upprepningar < 10)
+                        //En loop som lägger till ett heltal i variabeln varje varv tills vi når 9
+                        //(börjar räkna från 0 så det blir 10 loopar)
                         {
                             upprepningar++;
                             Console.WriteLine($"{upprepningar}. {upprepa}");
                         }
                         VäntaPåAnvändare();
                         break;
+
                     case "3":
                         Console.WriteLine("Skriv en mening med minst tre ord så visar vi dig det tredje ordet");
-                        Console.Write("Mening: ");
-                        string mening = Console.ReadLine();
-                        string[] ord = mening.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-                        // Vi använder TrimEntries och RemoveEmptyEntries tillsammans för att ta bort whitespace. Tack IntelliSense.
-                        Console.WriteLine($"Det tredje ordet är: {ord[2]}");
+                        do
+                        {
+                            Console.Write("Mening: ");
+                            string mening = Console.ReadLine();
+                            string[] ord = mening.Split(' ', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                            // Vi använder TrimEntries och RemoveEmptyEntries tillsammans för att ta bort whitespace. Tack IntelliSense.
+                            if (ord.Length < 3)
+                            {
+                                Console.WriteLine("Du måste skriva minst tre ord.");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Det tredje ordet är: {ord[2]}");
+                                break;
+                            }
+                        } while (true);
                         VäntaPåAnvändare();
+                        break;
+
+                    default:
                         break;
                 }
             } while (run);
         }
 
         static uint KöpBiljett()
+        //En metod för att köpa biljetter. Den returnerar kostnaden för biljetten så att vi kan använda den
+        //som input i en annan metod, metoden Sällskap där vi sammanslår priset för flera biljetter.
         {
+            //Eftersom dessa värden inte behöver vara ändringsbara/variabla i programmet kan vi deklarera dem som konstanter.
             const uint ungdomsPris = 80;
             const uint pensionärsPris = 90;
             const uint standardPris = 120;
             const uint barnPris = 0;
-            const uint näraDödenPris = 0;
+            const uint näraDödenPris = 0; // Vi skulle kunna slå ihop denna med barnPris och
+                                          // använda || i if-satsen men detta känns tydligare.
             do
             {
-                Console.Write("Ålder på besökare: ");
-                uint ålderUint = ValideraUINT();
-                if (ålderUint < 5)
+                uint ålderUint = ValideraUINT("Ålder på besökare: "); //Vi ser först till att användarens input är ett positiv heltal.
+                if (ålderUint < 5) //Sen matchar vi åldern med if-satser och returnerar priset från konstanterna ovan.
                 {
                     Console.WriteLine($"Barn under 5 år går gratis");
                     return barnPris;
                 }
-                if (ålderUint > 100)
+                else if (ålderUint > 100)
                 {
                     Console.WriteLine($"Pensionärer över 100 år går gratis");
                     return näraDödenPris;
@@ -106,34 +131,41 @@ namespace FlowStringmanipulation
         }
 
         static void Sällskap(uint input)
+        //Metoden tar emot hur många biljetter som ska köpas, visar detta för kunden tillsammans med kostnaden för alla biljetterna.
         {
             uint summa = 0;
-            for (int i = 0; i < input; i++)
+            //Vi kan jobba direkt i en heltalsvariabel eftersom vi inte har användning för en
+            // array eller lista enligt spec.
+            for (int i = 0; i < input; i++) 
             {
-                summa = summa + KöpBiljett();
+                summa += KöpBiljett();
             }
-            Console.WriteLine($"Antal personer i sällskapet: {input}");
+            Console.WriteLine($"\nAntal personer i sällskapet: {input}");
             Console.WriteLine($"Total kostnad för sällskapet: {summa}kr");
         }
 
-        static uint ValideraUINT()
+        static uint ValideraUINT(string inputBuffer)
         {
             do
             {
+                Console.Write(inputBuffer);
                 if (uint.TryParse(Console.ReadLine(), out uint output))
+                //TryParse returerar en sann boolean om dess input är ett positiv heltal
                 {
                     return output;
                 }
                 else
+                //Om input inte är ett positivt heltal så kör vi loopen igen och meddelar användaren om problemet.
                 {
-                    Console.WriteLine("Endast positiva heltal är tillåtna.");
+                    Console.WriteLine("Endast positiva heltal är tillåtna.\n");
                 }
             } while (true);
         }
 
         static void VäntaPåAnvändare()
+        //En enkel metod så att vi kan stanna upp programmet så att användaren hinner konfimera vad som händer.
         {
-            Console.WriteLine("Tryck på Enter för att återgå till huvudmenyn.");
+            Console.WriteLine("\nTryck på Enter för att återgå till huvudmenyn.");
             Console.ReadLine();
         }
     }
